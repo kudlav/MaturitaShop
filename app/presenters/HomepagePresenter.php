dc5df111ab59b490b0d098f60a452071fdb5e90b
Vladan Kudlac <vladankudlac@gmail.com>
2016-03-02 18:11:43 +0100

    Added Navbar control

--- app/presenters/HomepagePresenter.php
+++ app/presenters/HomepagePresenter.php
@@ -8,5 +8,9 @@ use App\Model;
 
 class HomepagePresenter extends BasePresenter
 {
-
+    protected function createComponentNavbar()
+    {
+        $control = new Navbar();
+        return $control;
+    }
 }
---
+++ app/presenters/templates/components/Navbar.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Presenters;
+
+use Nette\Application\UI\Control;
+
+
+class Navbar extends Control
+{
+    public function render()
+    {
+        $template = $this->template;
+        $template->setFile(__DIR__ . '/navbar.latte');
+
+        $template->render();
+    }
+}
