342e5a9d7da8b8225155cef95e5dd2d5517dceda
Vladan Kudlac <vladankudlac@gmail.com>
2016-04-02 00:27:35 +0200

    Registred user can use cart, saved DB.

--- app/model/CartManager.php
+++ app/model/CartManager.php
@@ -11,8 +11,11 @@ use Nette;
 class CartManager extends Nette\Object
 {
 	const
-		TABLE_NAME = 'baskets',
-		COLUMN_ID = 'users_id';
+		TABLE_BASKETS = 'baskets',
+		TABLE_PRODUCTS = 'products',
+		COLUMN_USERS_ID = 'users_id',
+		COLUMN_PRODUCTS_ID = 'products_id',
+		COLUMN_QUANTITY = 'quantity';
 
 
 	/** @var Nette\Database\Context */
@@ -26,12 +29,12 @@ class CartManager extends Nette\Object
 
 	/**
 	 * Return number of items in cart of user.
-	 * @param int $id
+	 * @param int $userId
 	 * @return int
 	 */
-	public function getCount($id)
+	public function getCount($userId)
 	{
-		$query = $this->database->table(self::TABLE_NAME)->where(self::COLUMN_ID, $id);
+		$query = $this->database->table(self::TABLE_BASKETS)->where(self::COLUMN_USERS_ID, $userId);
 
 		$ret = 0;
 		foreach ($query as $row) {
@@ -42,12 +45,12 @@ class CartManager extends Nette\Object
 
 	/**
 	 * Return total price of cart of user.
-	 * @param int $id
+	 * @param int $userId
 	 * @return int
 	 */
-	public function getPrice($id)
+	public function getPrice($userId)
 	{
-		$query = $this->database->table(self::TABLE_NAME)->where(self::COLUMN_ID, $id);
+		$query = $this->database->table(self::TABLE_BASKETS)->where(self::COLUMN_USERS_ID, $userId);
 
 		$ret = 0;
 		foreach ($query as $row) {
@@ -58,12 +61,12 @@ class CartManager extends Nette\Object
 
 	/**
 	 * Return array of items in cart of user.
-	 * @param int $id
+	 * @param int $userId
 	 * @return array
 	 */
-	public function getItems($id)
+	public function getItems($userId)
 	{
-		$query = $this->database->table(self::TABLE_NAME)->where(self::COLUMN_ID, $id);
+		$query = $this->database->table(self::TABLE_BASKETS)->where(self::COLUMN_USERS_ID, $userId);
 
 		$ret = array();
 		foreach ($query as $row) {
@@ -76,5 +79,47 @@ class CartManager extends Nette\Object
 			];
 		}
 		return $ret;
+	}
+
+	/**
+	 * Remove product from user cart.
+	 * @param $userId
+	 * @param $productId
+	 * @return int Return number of removed products.
+	 */
+	public function removeItem($userId, $productId)
+	{
+		$state = $this->database->table(self::TABLE_BASKETS)
+			->where(self::COLUMN_USERS_ID." = ? AND ".self::COLUMN_PRODUCTS_ID." = ?", $userId, $productId)
+			->delete();
+		return $state;
+	}
+
+	/**
+	 * Add product from database to user basket or increase quantity.
+	 * @param $userId
+	 * @param $productId
+	 * @return int Return number of added products.
+	 */
+	public function addItem($userId, $productId)
+	{
+			$product = $this->database->table(self::TABLE_PRODUCTS)->get($productId);
+			if ($product) {
+				$basket = $this->database->table(self::TABLE_BASKETS)
+					->where(self::COLUMN_USERS_ID." = ? AND ".self::COLUMN_PRODUCTS_ID." = ?", $userId, $productId)->fetch();
+				if ($basket) {
+					$basket->update(array(
+						'quantity' => $basket->quantity + 1,
+					));
+				} else {
+					$state = $this->database->table(self::TABLE_BASKETS)->insert(array(
+						self::COLUMN_USERS_ID => (int)$userId,
+						self::COLUMN_PRODUCTS_ID => (int)$productId,
+						self::COLUMN_QUANTITY => (int) 1,
+					));
+				}
+				return 1;
+			}
+			return 0;
 	}
 }
--- app/presenters/UserPresenter.php
+++ app/presenters/UserPresenter.php
@@ -55,4 +55,43 @@ class UserPresenter extends BasePresenter
 		}
 	}
 
+	/**
+	 * Remove selected product from cart
+	 * @param int $itemId
+	 */
+	public function actionRemoveFromCart($itemId)
+	{
+		if ($this->getUser()->isLoggedIn()) {
+			if ($this->cartManager->removeItem($this->getUser()->id, $itemId)) {
+				$this->flashMessage('Položka byla z košíku odebrána');
+			} else {
+				$this->flashMessage('Položku nebylo možné z košíku odebrat, patrně se v něm nenachází', 'flash-error');
+			}
+			$this->redirect('User:cart');
+		} else {
+			// TO-DO cart for unregistred users
+		}
+	}
+
+	/**
+	 * Add selected product to cart
+	 * @param int $itemId
+	 * @param $redirect
+	 */
+	public function actionAddToCart($itemId, $redirect)
+	{
+		if ($this->getUser()->isLoggedIn()) {
+			if ($this->cartManager->addItem($this->getUser()->id, $itemId)) {
+				$this->flashMessage('Položka byla přidána do košíku');
+			} else {
+				$this->flashMessage('Položku nebylo možné přidat do košíku', 'flash-error');
+			}
+		} else {
+			// TO-DO cart for unregistred users
+		}
+		if (isset($redirect)) {
+				$this->redirect($redirect);
+		}
+		$this->redirect('User:cart');
+	}
 }
