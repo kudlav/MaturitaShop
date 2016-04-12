3e99c37082ba1c1bb4e86f4051e78094be5e0fdb
Vladan Kudlac <vladankudlac@gmail.com>
2016-04-12 17:49:47 +0200

    Front:Info:nakup gets Delivery&Payment data from DB.

--- app/FrontModule/presenters/InfoPresenter.php
+++ app/FrontModule/presenters/InfoPresenter.php
@@ -2,11 +2,25 @@
 
 namespace App\FrontModule\Presenters;
 
+use App\FrontModule\Model\OrderManager;
 use Nette;
-use App\FrontModule\Model;
 
 
 class InfoPresenter extends BasePresenter
 {
+	/** @var  OrderManager */
+	private $orderManager;
 
+	public function __construct(OrderManager $orderManager)
+	{
+		parent::__construct();
+
+		$this->orderManager = $orderManager;
+	}
+
+	public function renderNakup()
+	{
+		$this->template->delivery = $this->orderManager->getDelivery();
+		$this->template->payment = $this->orderManager->getPayment();
+	}
 }
